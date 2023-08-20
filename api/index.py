b44b48550808964d52100fe60a2b6076e1071f5c
SilicoFlare <SilicoFlare@users.noreply.github.com>
2023-08-20 10:08:06 +0000

    Generated website files
    Signed-off-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- api/index.py
+++ api/index.py
@@ -9,33 +9,33 @@ components = {}
 
 @app.route('/')
 def index():
-    with open('api/static/data/index.json', 'r') as data:
+    with open('data/index.json', 'r') as data:
         components = json.loads(data.read())['components']
     return render_template('main.html', 
     project_name="Sample",
-    main_heading="HackerSpace",
-    content="Some random shit",
-    font_data="@import url('https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,400;0,700;1,400;1,700&family=Poppins&display=swap');",
-    body_classes="font-pop",
-    heading_classes="font-mono",
-    content_classes="font-pop",
-    subhead_classes="font-mono",
+    main_heading="",
+    content="",
+    stylesheets=['', ''],
+    body_classes="",
+    heading_classes="",
+    content_classes="",
+    subhead_classes="",
     components=components
     )
 
 @app.route('/newpage')
 def newpage():
-    with open('api/static/data/newpage.json', 'r') as data:
+    with open('data/newpage.json', 'r') as data:
         components = json.loads(data.read())['components']
     return render_template('main.html', 
     project_name="Sample",
-    main_heading="HackerSpace",
-    content="Some random shit",
-    font_data="@import url('https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,400;0,700;1,400;1,700&family=Poppins&display=swap');",
-    body_classes="font-pop",
-    heading_classes="font-mono",
-    content_classes="font-pop",
-    subhead_classes="font-mono",
+    main_heading="",
+    content="",
+    stylesheets=['', ''],
+    body_classes="",
+    heading_classes="",
+    content_classes="",
+    subhead_classes="",
     components=components
     )
 
