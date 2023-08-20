6c81f9953c9222111aca0805d00c4fbd7a3f1804
Suraj B M <silicoflare@gmail.com>
2023-08-20 19:54:53 +0530

    Added error handlers

--- api/index.py
+++ api/index.py
@@ -5,6 +5,69 @@ app = Flask(__name__)
 
 components = {}
 
+# ERROR LINKS
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('error.html',
+    title="Not Found",
+    project_name="Linkhub",
+    main_heading="404",
+    content="The requested URL was not found. Check the URL and try again.",
+    stylesheets=[
+        "<style>@import url('https://fonts.googleapis.com/css2?family=JetBrains+Mono&family=Poppins&display=swap');</style>", 
+        '<link rel="stylesheet" href="{{ url_for(\'static\', filename=\'linkhub.css\') }}"></link>'
+    ],
+    body_classes="font-pop",
+    heading_classes="font-hsp",
+    content_classes="font-pop",
+    subhead_classes="",
+    button_classes="rounded-md bg-black text-white",
+    components=[
+        {
+            "type": "heading",
+            "label": " "
+        },
+        {
+            "type": "button",
+            "label": "Go Back",
+            "properties": {
+                "link": "history.back()"
+            }
+        }
+    ]
+    )
+
+@app.errorhandler(500)
+def server_error(error):
+    return render_template('error.html',
+    title="Internal Server Error",
+    project_name="Linkhub",
+    main_heading="500",
+    content="There was some error with the server. Please try later, or check for errors.",
+    stylesheets=[
+        "<style>@import url('https://fonts.googleapis.com/css2?family=JetBrains+Mono&family=Poppins&display=swap');</style>", 
+        '<link rel="stylesheet" href="{{ url_for(\'static\', filename=\'linkhub.css\') }}"></link>'
+    ],
+    body_classes="font-pop",
+    heading_classes="font-hsp",
+    content_classes="font-pop",
+    subhead_classes="",
+    button_classes="rounded-md bg-black text-white",
+    components=[
+        {
+            "type": "heading",
+            "label": " "
+        },
+        {
+            "type": "button",
+            "label": "Go Back",
+            "properties": {
+                "link": "history.back()"
+            }
+        }
+    ]
+    )
+    
 # PAGE ENDPOINTS
 
 @app.route('/')
